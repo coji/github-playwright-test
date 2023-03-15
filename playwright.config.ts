8b58497cd5278281b1cc59e1075c968d0cc6ffc8
coji <coji@techtalk.jp>
2023-03-15 16:54:20 +0900

    bump

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,9 +1,9 @@
-import type { PlaywrightTestConfig } from "@playwright/test"
-import { devices } from "@playwright/test"
+import type { PlaywrightTestConfig } from '@playwright/test'
+import { devices } from '@playwright/test'
 
 // require('dotenv').config();
 const config: PlaywrightTestConfig = {
-  testDir: "./tests",
+  testDir: './tests',
   timeout: 30 * 1000,
   expect: {
     timeout: 5000
@@ -12,29 +12,24 @@ const config: PlaywrightTestConfig = {
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: "html",
+  reporter: 'html',
   use: {
     actionTimeout: 0,
-    trace: "retain-on-failure",
-    video: "retain-on-failure"
+    trace: 'retain-on-failure',
+    video: 'retain-on-failure'
   },
 
   projects: [
     {
-      name: "Mobile Chrome",
-      use: {
-        ...devices["Pixel 5"]
-      }
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] }
     },
     {
-      name: "Mobile Safari",
-      use: {
-        ...devices["iPhone 12"]
-      }
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] }
     }
   ],
-
-  outputDir: "test-results/"
+  outputDir: 'test-results/'
 }
 
 export default config
--- tests/techtalkjp.spec.ts
+++ tests/techtalkjp.spec.ts
@@ -1,53 +1,53 @@
-import { test, expect } from "@playwright/test"
+import { test, expect } from '@playwright/test'
 
-test("TechTalkJP website", async ({ page }) => {
-  await page.goto("https://www.techtalk.jp/")
+test('TechTalkJP website', async ({ page }) => {
+  await page.goto('https://www.techtalk.jp/')
 
   // フォームを埋める
-  await page.getByPlaceholder("お名前").fill("playwright")
-  await page.getByPlaceholder("会社名").fill("Playwright Corporation")
-  await page.getByPlaceholder("電話番号").fill("08000000000")
-  await page.getByPlaceholder("メール").fill("test@example.com")
+  await page.getByPlaceholder('お名前').fill('playwright')
+  await page.getByPlaceholder('会社名').fill('Playwright Corporation')
+  await page.getByPlaceholder('電話番号').fill('08000000000')
+  await page.getByPlaceholder('メール').fill('test@example.com')
   await page
-    .getByPlaceholder("メッセージ")
-    .fill("here is test message.\nthank you!")
+    .getByPlaceholder('メッセージ')
+    .fill('here is test message.\nthank you!')
 
   // フォームを送信
-  await page.getByRole("button", { name: "Let's talk" }).click()
+  await page.getByRole('button', { name: "Let's talk" }).click()
 
   // フォームが送信されたことを確認
   // await except じゃないと visible になるのを待たない！
   await expect(
     await page.getByText(
-      "お問い合わせありがとうございます。以下のメッセージを受付けました。お返事をお待ち下さい。"
+      'お問い合わせありがとうございます。以下のメッセージを受付けました。お返事をお待ち下さい。'
     )
   ).toBeVisible()
 })
 
-test("TechTalkJP website in English", async ({ page }) => {
-  await page.goto("https://www.techtalk.jp/")
+test('TechTalkJP website in English', async ({ page }) => {
+  await page.goto('https://www.techtalk.jp/')
 
   // English を選択
-  await page.getByRole("button", { name: "Language" }).click()
-  await page.getByRole("menuitem", { name: "English" }).click()
-  await expect(page).toHaveURL("https://www.techtalk.jp/en")
+  await page.getByRole('button', { name: 'Language' }).click()
+  await page.getByRole('menuitem', { name: 'English' }).click()
+  await expect(page).toHaveURL('https://www.techtalk.jp/en')
 
   // フォームを埋める
-  await page.getByPlaceholder("Name").fill("playwright")
-  await page.getByPlaceholder("Company").fill("Playwright company")
-  await page.getByPlaceholder("Phone").fill("08000000000")
-  await page.getByPlaceholder("Email").fill("test@example.com")
+  await page.getByPlaceholder('Name').fill('playwright')
+  await page.getByPlaceholder('Company').fill('Playwright company')
+  await page.getByPlaceholder('Phone').fill('08000000000')
+  await page.getByPlaceholder('Email').fill('test@example.com')
   await page
-    .getByPlaceholder("Message")
-    .fill("this is test for english pages\nthank you!")
+    .getByPlaceholder('Message')
+    .fill('this is test for english pages\nthank you!')
 
   // フォームを送信
-  await page.getByRole("button", { name: "Let's talk" }).click()
+  await page.getByRole('button', { name: "Let's talk" }).click()
 
   // フォームが送信されたことを確認
   await expect(
     await page.getByText(
-      "お問い合わせありがとうございます。以下のメッセージを受付けました。お返事をお待ち下さい。"
+      'お問い合わせありがとうございます。以下のメッセージを受付けました。お返事をお待ち下さい。'
     )
   ).toBeVisible()
 })
